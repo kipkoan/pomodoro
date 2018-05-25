30da786f06ab0649bbcc45f0f79e063069e56abb
Kip McEntire <kipkoan@gmail.com>
2018-05-25 13:08:03 -0700

    replace tabs with spaces

--- pomodoro.sh
+++ pomodoro.sh
@@ -6,19 +6,19 @@ source ~/.secrets
 date="/usr/local/bin/gdate"
 
 curl -X POST -H "Content-Type: application/json" \
-	-H "Authorization: Bearer ${SLACK_TOKEN}" \
-	-d '{"profile": {"status_emoji": ":tomato:", "status_text": "Pomodoro → '$(echo $(${date} -d "+25 minutes" +%l:%M%P))'"}}' \
-	-so /dev/null \
-	"https://slack.com/api/users.profile.set"
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
+    -d '{"profile": {"status_emoji": ":tomato:", "status_text": "Pomodoro → '$(echo $(${date} -d "+25 minutes" +%l:%M%P))'"}}' \
+    -so /dev/null \
+    "https://slack.com/api/users.profile.set"
 
 curl -X GET -H "Authorization: Bearer ${SLACK_TOKEN}" \
-	-so /dev/null \
-	"https://slack.com/api/dnd.setSnooze?num_minutes=25"
+    -so /dev/null \
+    "https://slack.com/api/dnd.setSnooze?num_minutes=25"
 
 sleep 1500
 
 curl -X POST -H "Content-Type: application/json" \
-	-H "Authorization: Bearer ${SLACK_TOKEN}" \
-	-d '{"profile": {"status_emoji": "", "status_text": ""}}' \
-	-so /dev/null \
-	"https://slack.com/api/users.profile.set"
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
+    -d '{"profile": {"status_emoji": "", "status_text": ""}}' \
+    -so /dev/null \
+    "https://slack.com/api/users.profile.set"
